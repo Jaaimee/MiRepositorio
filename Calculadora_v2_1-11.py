ca97649076e32f75c481d25184be225d88551b4b
Jaime Párraga Plaza <2021354@iesluisbraille.es>
2022-01-27 10:18:24 +0100

    Versión 5

--- Calculadora_v2_1-11.py
+++ Calculadora_v2_1-11.py
@@ -61,5 +61,10 @@ while (o!=12):
         y=int(input("Escribe el segundo número: "))
         division(x, y)
         
+    elif (o == 5):
+         x=int(input("Escribe el primer número: "))
+         y=int(input("Escribe el segundo número: "))
+         cociente(x, y)
+         
     break
     
